34c9640df96948b2cf54ff511fe1811d8a0a9881
34Ferdinand <marianrotaru@live.com>
2026-07-18 23:01:57 +0100

    Update sources.js

--- sources.js
+++ sources.js
@@ -1,20 +1,21 @@
 const NEWS_SOURCES = {
+
   world: [
     {
-      name: "BBC News",
-      feed: "https://feeds.bbci.co.uk/news/world/rss.xml"
+      name: "The Guardian",
+      feed: "https://www.theguardian.com/world/rss"
     },
     {
-      name: "AP News",
+      name: "Euronews",
+      feed: "https://www.euronews.com/rss?level=theme&name=news"
+    },
+    {
+      name: "Associated Press",
       feed: "https://news.google.com/rss/search?q=site%3Aapnews.com&hl=en-GB&gl=GB&ceid=GB%3Aen"
     },
     {
       name: "The Kyiv Independent",
       feed: "https://kyivindependent.com/feed/"
-    },
-    {
-      name: "Euronews",
-      feed: "https://www.euronews.com/rss?level=theme&name=news"
     },
     {
       name: "Al Jazeera",
@@ -28,12 +29,12 @@ const NEWS_SOURCES = {
       feed: "https://feeds.arstechnica.com/arstechnica/index"
     },
     {
-      name: "The Verge",
-      feed: "https://www.theverge.com/rss/index.xml"
-    },
-    {
       name: "TechCrunch",
       feed: "https://techcrunch.com/feed/"
+    },
+    {
+      name: "The Verge",
+      feed: "https://www.theverge.com/rss/index.xml"
     },
     {
       name: "MacRumors",
@@ -47,16 +48,13 @@ const NEWS_SOURCES = {
 
   sport: [
     {
-      name: "BBC Sport",
-      feed: "https://feeds.bbci.co.uk/sport/rss.xml?edition=uk"
+      name: "The Guardian Sport",
+      feed: "https://www.theguardian.com/uk/sport/rss"
     },
     {
-      name: "The Athletic",
-      feed: "https://news.google.com/rss/search?q=site%3Atheathletic.com&hl=en-GB&gl=GB&ceid=GB%3Aen"
-    },
-    {
-      name: "ATP Tour",
-      feed: "https://news.google.com/rss/search?q=site%3Aatptour.com&hl=en-GB&gl=GB&ceid=GB%3Aen"
+      name: "Eurosport",
+      feed: "https://www.eurosport.com/rss.xml"
     }
   ]
+
 };
